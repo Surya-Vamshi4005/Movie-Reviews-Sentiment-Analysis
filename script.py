33fbbe305d505e9756660025cf36b66df5d46027
Surya Vamshi <85131189+Surya-Vamshi4005@users.noreply.github.com>
2023-11-15 10:51:22 +0530

    File Upload

---
+++ script.py
@@ -0,0 +1,11 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import re # for regex
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import SnowballStemmer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB,MultinomialNB,BernoulliNB
+from sklearn.metrics import accuracy_score
+import pickle
